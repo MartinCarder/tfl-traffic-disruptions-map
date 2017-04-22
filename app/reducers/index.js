ddcbdf313ebac0f009ec6731a7969189933c3d08
Martin Carder <mcarder@syzygy.co.uk>
2017-04-22 22:26:27 +0100

    intial setup of reducers

---
+++ app/reducers/index.js
@@ -0,0 +1,39 @@
+import { combineReducers } from 'redux';
+import {
+  FETCH_TRAFFIC_REQUEST,
+  FETCH_TRAFFIC_FAILURE,
+  FETCH_TRAFFIC_SUCCESS,
+} from '../actions/actions';
+
+const trafficData = (state = {
+  isFetching: false,
+  isError: false,
+  data: [],
+}, action) => {
+  switch (action.type) {
+    case FETCH_TRAFFIC_REQUEST:
+      return Object.assign({}, state, {
+        isFetching: true,
+        isError: false,
+      });
+    case FETCH_TRAFFIC_FAILURE:
+      return Object.assign({}, state, {
+        isFetching: false,
+        isError: true,
+      });
+    case FETCH_TRAFFIC_SUCCESS:
+      return Object.assign({}, state, {
+        isFetching: false,
+        isError: false,
+        data: action.data,
+      });
+    default:
+      return state;
+  }
+};
+
+const rootReducer = combineReducers({
+  trafficData,
+});
+
+export default rootReducer;
