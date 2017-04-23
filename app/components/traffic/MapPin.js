c443b97127cf0bbfe0627a1fd420d112bdd164db
Martin Carder <mcarder@syzygy.co.uk>
2017-04-23 15:21:45 +0100

    handle pin move/removal

--- app/components/traffic/MapPin.js
+++ app/components/traffic/MapPin.js
@@ -4,12 +4,28 @@ const propTypes = {
   mapRef: React.PropTypes.object,
   lat: React.PropTypes.number.isRequired,
   lng: React.PropTypes.number.isRequired,
+  pinCallback: React.PropTypes.func.isRequired,
+  popupMarkup: React.PropTypes.string.isRequired,
 };
 
 class MapPin extends React.Component {
   componentDidMount() {
     this.pinClick = this.pinClick.bind(this);
     this.addPin();
+  }
+
+  componentDidUpdate(prevProps) {
+    const { lng, lat } = this.props;
+    if (lng !== prevProps.lng || lat !== prevProps.lat) {
+      const latlng = new window.google.maps.LatLng(lat, lng);
+      this.marker.setPosition(latlng);
+    }
+  }
+
+  componentWillUnmount() {
+    this.marker.removeListener('click', this.pinClick);
+    this.marker.setMap(null);
+    this.marker = null;
   }
 
   addPin() {
